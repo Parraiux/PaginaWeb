eb0ac72fc2fe2a8ea4f52898f239989dd29f5de5
Parriux <jparra11332@ufide.ac.cr>
2023-10-06 10:27:59 -0400

    TareaWeb

---
+++ PaginaParra/src/main/java/Tarea/demo/PaginaWebApplication.java
@@ -0,0 +1,13 @@
+package Tarea.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaWebApplication.class, args);
+	}
+
+}
---
+++ PaginaParra/src/test/java/Tarea/demo/PaginaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package Tarea.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
